3a9ada3b3010059359469cbf1d31b7eac79e51e2
AtomToast <reg-2@t-online.de>
2020-09-13 14:56:38 +0200

    add khal config

---
+++ .config/khal/colors.py
@@ -0,0 +1,94 @@
+# Copyright (c) 2013-2017 Christian Geier et al.
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+dark = [
+    ('header', 'light gray', 'dark gray', 'bold'),
+    ('footer', 'light gray', 'dark gray', 'bold'),
+    ('line header', 'black', 'white', 'bold'),
+    ('bright', 'dark blue', 'white', ('bold', 'standout')),
+    ('list', 'black', 'white'),
+    ('list focused', 'white', 'light blue', 'bold'),
+    ('edit', 'black', 'white'),
+    ('edit focused', 'white', 'light blue', 'bold'),
+    ('button', 'black', 'dark cyan'),
+    ('button focused', 'white', 'light blue', 'bold'),
+
+    ('reveal focus', 'black', 'brown'),
+    ('today focus', 'white', 'dark magenta'),
+    ('today', 'dark gray', 'dark green',),
+
+    ('date header', 'light red', 'black'),
+    ('date header focused', 'black', 'yellow'),
+    ('date header selected', 'dark gray', 'brown'),
+
+    ('dayname', 'light magenta', ''),
+    ('monthname', 'dark green', ''),
+    ('weeknumber_right', 'light gray', ''),
+    ('edit', 'black', 'dark blue'),
+    ('alert', 'white', 'dark red'),
+    ('mark', 'black', 'light magenta', 'bold'),
+    ('frame', 'white', 'black'),
+    ('frame focus', 'light red', 'black'),
+    ('frame focus color', 'light green', 'black'),
+    ('frame focus top', 'dark magenta', 'black'),
+
+    ('editfc', 'white', 'dark blue', 'bold'),
+    ('editbx', 'light gray', 'dark blue'),
+    ('editcp', 'black', 'light gray', 'standout'),
+    ('popupbg', 'white', 'black', 'bold'),
+]
+light = [
+    ('header', 'black', 'white'),
+    ('footer', 'black', 'white'),
+    ('line header', 'black', 'white', 'bold'),
+    ('bright', 'dark blue', 'white', ('bold', 'standout')),
+    ('list', 'black', 'white'),
+    ('list focused', 'white', 'light blue', 'bold'),
+    ('edit', 'black', 'white'),
+    ('edit focused', 'white', 'light blue', 'bold'),
+    ('button', 'black', 'dark cyan'),
+    ('button focused', 'white', 'light blue', 'bold'),
+
+    ('reveal focus', 'black', 'dark cyan', 'standout'),
+    ('today focus', 'white', 'dark cyan', 'standout'),
+    ('today', 'black', 'light gray', 'dark cyan'),
+
+    ('date header', '', 'white'),
+    ('date header focused', 'white', 'dark gray', ('bold', 'standout')),
+    ('date header selected', 'dark gray', 'light cyan'),
+
+    ('dayname', 'dark gray', 'white'),
+    ('monthname', 'dark gray', 'white'),
+    ('weeknumber_right', 'dark gray', 'white'),
+    ('edit', 'white', 'dark blue'),
+    ('alert', 'white', 'dark red'),
+    ('mark', 'white', 'dark green'),
+    ('frame', 'dark gray', 'white'),
+    ('frame focus', 'light red', 'white'),
+    ('frame focus color', 'dark blue', 'white'),
+    ('frame focus top', 'dark magenta', 'white'),
+
+    ('editfc', 'white', 'dark blue', 'bold'),
+    ('editbx', 'light gray', 'dark blue'),
+    ('editcp', 'black', 'light gray', 'standout'),
+    ('popupbg', 'white', 'black', 'bold'),
+]
