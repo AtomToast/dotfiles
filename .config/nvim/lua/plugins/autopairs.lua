90ebf087ce65dbcb6b415fb347fe1f1d4983fe99
AtomToast <reg-2@t-online.de>
2024-10-27 18:02:32 +0100

    nvim: update zls config

--- .config/nvim/lua/plugins/autopairs.lua
+++ .config/nvim/lua/plugins/autopairs.lua
@@ -3,7 +3,13 @@ return {
   -- Optional dependency
   dependencies = { 'hrsh7th/nvim-cmp' },
   config = function()
-    require('nvim-autopairs').setup {}
+    local npairs = require 'nvim-autopairs'
+    local Rule = require 'nvim-autopairs.rule'
+    npairs.setup { check_ts = true }
+
+    local ts_conds = require 'nvim-autopairs.ts-conds'
+    npairs.add_rule(Rule('|', '|', 'zig'):with_pair(ts_conds.is_ts_node { 'WhileStatement', 'IfStatement' }))
+
     -- If you want to automatically add `(` after selecting a function or method
     local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
     local cmp = require 'cmp'
