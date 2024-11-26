c79685640e54870761aa6bf9ad30933f8d89938b
Kjell Hoffhenke <kjell.hoffhenke@ki-elements.de>
2024-11-26 17:08:39 +0100

    nvim: update which-key config format

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -2,14 +2,11 @@ return {
   {
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-    config = function()
-      require('which-key').setup()
-
-      -- Document existing key chains
-      require('which-key').register {
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = '[H]unk', _ = 'which_key_ignore' },
-      }
-    end,
+    keys = {
+      { '<leader>h', group = '[H]unk' },
+      { '<leader>h_', hidden = true },
+      { '<leader>s', group = '[S]earch' },
+      { '<leader>s_', hidden = true },
+    },
   },
 }
