9423157b3d109842fb1dca2144cba56517213753
AtomToast <reg-2@t-online.de>
2025-07-03 14:29:27 +0200

    nvim: gruvbox fix floating window background with transparency

--- .config/nvim/lua/plugins/gruvbox.lua
+++ .config/nvim/lua/plugins/gruvbox.lua
@@ -1,16 +1,22 @@
 return {
   {
     'ellisonleao/gruvbox.nvim',
-    opts = {
-      transparent_mode = true,
-      overrides = {
-        -- These seem to not really be maintained as part of the colorscheme
-        DapBreakpointSymbol = { link = 'GruvboxRedSign' },
-        DapStoppedSymbol = { link = 'GruvboxGreenSign' },
-      },
-    },
     priority = 1000, -- make sure to load this before all the other start plugins
-    init = function()
+    config = function()
+      require('gruvbox').setup {
+        transparent_mode = true,
+        overrides = {
+          -- These seem to not really be maintained as part of the colorscheme
+          DapBreakpointSymbol = { link = 'GruvboxRedSign' },
+          DapStoppedSymbol = { link = 'GruvboxGreenSign' },
+          -- No transparency on floating windows
+          NormalFloat = {
+            bg = require('gruvbox').palette['dark1'],
+            fg = require('gruvbox').palette['light1'],
+          },
+        },
+      }
+
       vim.cmd.colorscheme 'gruvbox'
     end,
   },
