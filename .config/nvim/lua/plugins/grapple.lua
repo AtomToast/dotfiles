25e25ce1d15722e2c51094d909346b54b9416a27
AtomToast <reg-2@t-online.de>
2025-07-03 14:23:47 +0200

    nvim: change grapple shortcut
    <A-S-Number> does not seem to work properly

--- .config/nvim/lua/plugins/grapple.lua
+++ .config/nvim/lua/plugins/grapple.lua
@@ -7,7 +7,7 @@ return {
       vim.keymap.set('n', '<A-0>', '<cmd>Grapple toggle_tags<CR>', { desc = 'Show Tags list' })
       for tag = 1, 9 do
         vim.keymap.set('n', '<A-' .. tag .. '>', '<cmd>Grapple select index=' .. tag .. '<CR>', { desc = 'Select tag ' .. tag })
-        vim.keymap.set('n', '<A-S-' .. tag .. '>', '<cmd>Grapple tag index=' .. tag .. '<CR>', { desc = 'Tag buffer as ' .. tag })
+        vim.keymap.set('n', '<leader>' .. tag, '<cmd>Grapple tag index=' .. tag .. '<CR>', { desc = 'Tag buffer as ' .. tag })
       end
     end,
   },
