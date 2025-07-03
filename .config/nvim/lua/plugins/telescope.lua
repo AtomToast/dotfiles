37fcdba510730ab97efefdfb7b2bac5150c04445
AtomToast <reg-2@t-online.de>
2025-07-03 14:23:56 +0200

    nvim: fix telescope-fzf

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -23,6 +23,7 @@ return {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
           },
+          fzf = {},
         },
         defaults = {
           mappings = {
