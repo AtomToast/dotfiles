6418aaebfa75670776f60da12a4ba9ceecc40419
AtomToast <reg-2@t-online.de>
2025-07-03 14:23:52 +0200

    nvim: add treesitter javascript syntax

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'html', 'javascript', 'lua', 'markdown', 'vim', 'vimdoc' },
       auto_install = true,
       highlight = {
         enable = true,
