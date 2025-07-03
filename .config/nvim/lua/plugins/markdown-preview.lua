8c473bca383f58d902c7eb03d712b9b6ee6a1908
AtomToast <reg-2@t-online.de>
2025-07-03 14:23:47 +0200

    nvim: add markdown-preview

---
+++ .config/nvim/lua/plugins/markdown-preview.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    'iamcco/markdown-preview.nvim',
+
+    build = 'cd app && npm install',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    init = function()
+      vim.g.mkdp_filetypes = { 'markdown' }
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = { 'markdown' },
+        callback = function()
+          vim.keymap.set('n', '<leader>mp', '<cmd>MarkdownPreviewToggle<CR>', {
+            buffer = true,
+            desc = 'Toggle Markdown Preview',
+          })
+        end,
+      })
+    end,
+    ft = { 'markdown' },
+  },
+}
